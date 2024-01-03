e9aed56ac8c8021b0a6f0d7c85c699be6bea3e55
Adam Salih <adam.salih@cleevio.com>
2024-01-03 16:59:28 +0100

    Change package reference to url

--- Package.swift
+++ Package.swift
@@ -16,8 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "InheritableEnum", path: "../InheritableEnum")
+        .package(url: "https://github.com/max9631/InheritableEnum.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
